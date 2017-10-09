b8fc7b07e1fa772e32b47f1b68fa788f33286353
andrewchen44 <andrewchen.c44@gmail.com>
2017-10-09 16:10:50 -0700

    done with stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,63 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+if (obj === null ) {
+
+  return 'null';
+}
+// 
+if (typeof obj === 'string') {
+  return "\"" + obj + "\"";
+}
+// 
+if(typeof obj === 'function' || typeof obj === 'undefined') {
+  return ;
+}
+
+if(typeof obj === 'boolean') {
+  if(obj === false){
+    return 'false';
+}
+  return 'true';
+}
+
+if(Array.isArray(obj) && obj.length === 0){
+  return '[]';
+ } else if (Array.isArray(obj)) {
+    var output = [];
+    obj.forEach(function(item){
+      output.push(stringifyJSON(item));
+    });
+    return '[' + output + ']';
+
+}
+
+if(typeof obj === 'object') {
+  var result = '';
+  var getkeys = Object.keys(obj);
+    if(getkeys.length === 0) {
+      return '{}';
+    }
+  
+  getkeys.forEach(function(item,idx){
+    if(item === 'functions'|| item === 'undefined'){
+      return;
+    } else if(idx === getkeys.length-1){
+        result += stringifyJSON(item) + ":" + stringifyJSON(obj[item])
+    } else {
+        result += stringifyJSON(item) + ":" + stringifyJSON(obj[item])+ ","
+           }
+  });
+return '{'+ result + '}';
+}
+
+  //go through all the properties in the object
+  //add the stringified key to the result
+  //add the colon to the results
+  //add the stringified value to the result
+  //add a comma after the value
+  //return the results surrounded by the brackets within quotes
+  
+return '' + obj;
 };
